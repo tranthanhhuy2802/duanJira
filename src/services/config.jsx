8bf917e8ed4878be40b78cb1642b45e4226402a5
Huy <72079366+tranthanhhuy2802@users.noreply.github.com>
2024-02-26 22:39:19 +0700

    Update config.jsx

--- src/services/config.jsx
+++ src/services/config.jsx
@@ -4,7 +4,7 @@ import { getDuLieuLocal } from "../utils/localStore";
 const BASE_URL = "https://jiranew.cybersoft.edu.vn/";
 
 const TokenCybersoft =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0OCIsIkhldEhhblN0cmluZyI6IjEzLzAzLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMDI4ODAwMDAwMCIsIm5iZiI6MTY3OTY3NzIwMCwiZXhwIjoxNzEwNDM1NjAwfQ.lSPZOlcWH-N1gVY8QLGpPHPcKYEMpoh0FJuMEJbeJQQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0OSIsIkhldEhhblN0cmluZyI6IjA2LzAzLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwOTY4MzIwMDAwMCIsIm5iZiI6MTY4MjYxNDgwMCwiZXhwIjoxNzA5ODMwODAwfQ.k43D4dhebGpNofw1VImBYXXnqBcxtrDhQaHzcaN4mr8";
 
 const tokkenAuthorization = getDuLieuLocal("user");
 const configHeaderAxios = () => {
